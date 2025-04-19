facc20f08c06c0b8805198b51fb35c0bfb3cd801
EduFkn <ploutzin129@gmail.com>
2025-04-19 15:17:29 -0300

    calculator

---
+++ Calculator/script.js
@@ -0,0 +1,18 @@
+function adicionarCaracter(caracter){
+  const valorinput= document.querySelector('.display').value
+  
+  document.querySelector('.display').value = valorinput + caracter
+
+}
+function limparTela(){
+
+  document.querySelector('.display').value=''
+}
+function calcular(){
+  const valorinput= document.querySelector('.display').value
+   document.querySelector('.display').value=eval(valorinput)
+}
+function invert(){
+  const valorinput= document.querySelector('.display').value
+  document.querySelector('.display').value=valorinput*-1
+}
--- sei-la/script.js
+++
@@ -1,5 +0,0 @@
-let nota1= number(prompt("Digite a primeira nota"));
-let nota2= number(prompt("Digite a segunda nota"));
-
-let resultado= (nota1+nota2) /2;
-alert(resultado>=5);
